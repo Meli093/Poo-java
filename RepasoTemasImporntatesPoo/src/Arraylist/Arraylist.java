116751a2a1a9028152422cbcb0c39f6645c5db67
MR <rutilio_3190@hotmail.com>
2018-08-01 19:52:46 -0500

    array

---
+++ RepasoTemasImporntatesPoo/src/Arraylist/Arraylist.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arraylist;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+/**
+ *
+ * @author MR
+ */
+public class Arraylist {
+public static void main(String[] args) {
+String miarray[]={"jose","lopez","Vazquez"};
+for(int x=0;x<miarray.length;x++){
+    System.out.println(miarray[x]);    
+}        
+    }
+}
--- RepasoTemasImporntatesPoo/src/CuestiionarioProyecto/AplicacionPreguntas.java
+++
@@ -1,8 +0,0 @@
-package CuestiionarioProyecto;
-import java.util.*;
-public class AplicacionPreguntas {
-public static void main(String[] args) {
-Opcion O11=new Opcion("Cinematico", true);
-    
-
-}}
